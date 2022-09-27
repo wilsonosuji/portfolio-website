21358d55acfcabf280635b3222b89a8f56b58707
Wilson Osuji <90177009+wilsonosuji@users.noreply.github.com>
2022-09-27 11:58:24 +0100

    portfolio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/pg.jpg'
+import ME from '../../assets/profile-pic.jpg'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
